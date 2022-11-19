fd92110f4954ed1c4486cf464be499cdd2f39985
Kenno-Warise <wariken0523@gmail.com>
2022-11-19 14:12:53 +0900

    システムを実行した時の表示内容の見栄えを整える

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import speech_recognition as sr
 import pyttsx3
 
 
-quit_list = ['終了', '辞める', 'さようなら']
+quit_list = ['終了', 'やめる', '辞める', 'さようなら']
 input_value = input('----------\n音声認識の場合はそのまま「Enter」キーを押してください。\nテキスト入力の場合は何か文字を入力してから「Enter」キーを押してください\n----------\n')
 
 if input_value:
-    print('テキスト入力してください')
+    print('テキスト入力してください---->')
     print()
     
     while True:
@@ -26,13 +26,12 @@ if input_value:
             # 何も入力されなかった場合は以下を返す
             someting = 'テキストを入力してください'
             print(someting)
+            print()
             engine.say(someting)
             engine.runAndWait()
 else:
-    print('音声認識モード準備中です')
-
-    # r = sr.Recognizer() # 文字起こし
-    # engine = pyttsx3.init() # 文字の読み出し
+    print('音声認識モード準備中です---->')
+    print()
     
     while True:
         r = sr.Recognizer() # 文字起こしオブジェクトの初期化
@@ -45,6 +44,7 @@ else:
             # 音声認識されたらその文言を音声合成により読み出す
             text = r.recognize_google(audio, language='ja-JP')
             print(text)
+            print()
             engine.say(text)
             if text in quit_list:
                 # 音声認識された文言がquit_list内にあれば無限ループが停止する
@@ -53,7 +53,8 @@ else:
             engine.runAndWait()
         except:
             # 音声認識が何もされなければ、発言を促さすように以下のストリングを読み出す
-            someting = '何か発音してください'
+            someting = '何か発言してください'
             print(someting)
+            print()
             engine.say(someting)
             engine.runAndWait()
